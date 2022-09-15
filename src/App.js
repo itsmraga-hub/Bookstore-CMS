fb9a5926af4b0870e3e88178f0511783fa92032a
Raga <ragawilliam570@gmail.com>
2022-09-15 13:02:59 +0300

    Add App.js that brings all compoents together to form single page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import BookStore from './components/BookStore';
+import Header from './components/Header';
+import './css/App.css';
+
+const App = () => (
+  <>
+    <Header />
+    <BookStore />
+  </>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './App';
+import Categories from './routes/Categories';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Router>
-//     <Routes>
-//       <Route path="/" element={<App />} />
-//       <Route path="calculator" />
-//     </Routes>
-//   </Router>,
-// );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </Router>
+    ,
   </React.StrictMode>,
 );
