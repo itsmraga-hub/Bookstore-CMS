3572957fa6c4bc958b8d5e672620cc1a8b0da80c
Raga <ragawilliam570@gmail.com>
2022-09-19 19:35:20 +0300

    Import useDispatch and dispatch action to remove book

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+// Redux functions
+import { useDispatch } from 'react-redux';
+
+import store from '../reducers/configureStore';
+
+import { removeBook } from '../reducers/books/books';
+
+// Style sheets
 import style from './css/BookItem.module.css';
 
 const BookItem = (props) => {
   const book = props;
-  const { title, author } = book;
+  const { title, author, id } = book;
+
+  const dispatch = useDispatch();
+  const { books } = store.getState();
+
+  const getBook = (e) => {
+    e.preventDefault();
+    const bk = books.filter((bk) => bk.id === e.target.id);
+    dispatch(removeBook(bk[0]));
+  };
+
   return (
     <li className={style.BookContainer}>
       <div className={style.BookInfo}>
@@ -13,7 +31,7 @@ const BookItem = (props) => {
         <p className={style.BookAuthor}>{author}</p>
         <ul>
           <li><a href="./">Comments</a></li>
-          <li><a href="./">Remove</a></li>
+          <li><a id={id} onClick={getBook} href="./">Remove</a></li>
           <li><a href="./">Edit</a></li>
         </ul>
       </div>
