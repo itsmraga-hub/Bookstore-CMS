f655b409925bc07bb79c3cd438882c7c4f8176f1
Raga <ragawilliam570@gmail.com>
2022-09-19 13:29:22 +0300

    Import provider and add store to App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// React-redux
+import { Provider } from 'react-redux';
+import store from './reducers/configureStore';
+
 import App from './App';
 import Categories from './routes/Categories';
 
@@ -9,12 +13,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="categories" element={<Categories />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </Provider>
     ,
   </React.StrictMode>,
 );
