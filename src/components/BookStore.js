6012535220fce88537dcb6d64893fe06391be2cc
Raga <ragawilliam570@gmail.com>
2022-09-20 16:48:22 +0300

    Modify BookStore to receive state from API

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import BookItem from './BookItem';
+
+import { fetchBooks } from '../reducers/books/books';
 
 import style from './css/BookStore.module.css';
 import InputBook from './InputBook';
 
 const BookStore = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
+
   const books = useSelector((state) => state.books);
   return (
     <>
